05c51f5911bfe074c7c17b2723340f4be81ef766
Gregor Reitzenstein <me@dequbed.space>
2019-04-07 09:04:31 +0000

    Shake should now properly recompile on updates

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,26 @@ import Hibachi.Post
 import Hibachi.Style
 
 import Data.Text.Lazy.IO (writeFile)
+import Data.HashMap.Strict (empty)
 
 main :: IO ()
-main = shakeArgs shakeOptions $ do
+main = do
+    shakeArgs shakeOptions{ shakeExtra = setRepoPath "/home/glr/Documents/Blog/posts" empty } defs
+
+defs :: Rules ()
+defs = do
+    setupHibachi
+
     want ["out/css/default.css", "out/index.html", "posts"]
 
     "out/css/default.css" %> \o -> liftIO $ writeFile o styleText
 
     "out/index.html" %> \p -> do
+        oid <- gitRefNeed "refs/heads/posts"
         ps <- liftIO $ indexPosts Nothing "/home/glr/Documents/Blog/posts"
         liftIO $ writeFile p $ generateIndex ps
+
     "posts" ~> do
+        oid <- gitRefNeed "refs/heads/posts"
         ps <- liftIO $ posts Nothing "/home/glr/Documents/Blog/posts"
         liftIO $ mapM_ writePost ps
--- src/Hibachi/Shake.hs
+++ src/Hibachi/Shake.hs
@@ -1,26 +1,70 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Hibachi.Shake
+    ( gitRefNeed
+    , gitResolveReference
+    , setupHibachi
+    , setRepoPath
+    )
     where
 
 import Development.Shake
 import Development.Shake.Rule
 import Development.Shake.Classes
 
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+
+import Data.Maybe
+
+import Data.Typeable (TypeRep)
+import Data.Dynamic (Dynamic)
+import Data.HashMap.Strict (HashMap)
+
+import Git
+import Git.Types
+import Git.Libgit2 (lgFactory)
+
 import Hibachi.Post
 
-newtype PostR = PostR String
+type RefOid = Text
+
+newtype RepoPath = RepoPath { asPath :: FilePath }
+    deriving (Show, Eq)
+
+newtype GitRef = GitRef RefName
     deriving (Show, Eq, Hashable, Binary, NFData)
-type instance RuleResult PostR = Post
+type instance RuleResult GitRef = RefOid
 
-data PostRule = PostRule PostR (Action ())
+gitRefNeed :: RefName -> Action RefOid
+gitRefNeed = apply1 . GitRef
 
-postRule :: String -> Action () -> Rules ()
-postRule title act = addUserRule $ PostRule (PostR title) act
+addBuiltinGitRefRule :: Rules ()
+addBuiltinGitRefRule = addBuiltinRule noLint noIdentity run
+    where
+        run :: BuiltinRun GitRef RefOid
+        run key old mode = do
+            now <- gitResolveReference key
+            let nowbs = T.encodeUtf8 now
+            if mode == RunDependenciesSame && old == Just nowbs then
+                return $ RunResult ChangedNothing nowbs now
+            else
+                return $ RunResult ChangedRecomputeDiff nowbs now
 
-postNeed :: String -> Action Post
-postNeed = apply1 . PostR
+gitResolveReference :: GitRef -> Action RefOid
+gitResolveReference (GitRef ref) = do
+    repopath <- getShakeExtra
+    path <- case repopath of
+        Just p -> return $ asPath p
+        Nothing -> error "Repository path is not set. Did you set the path in your Shake args?"
+    liftIO $ withRepository lgFactory path $ do
+        refhead <- resolveReference ref
+        return $ renderOid $ fromJust refhead
 
-needAllPosts :: Action [Post]
-needAllPosts = undefined
+setupHibachi :: Rules ()
+setupHibachi = do
+    addBuiltinGitRefRule
 
+setRepoPath :: FilePath -> HashMap TypeRep Dynamic -> HashMap TypeRep Dynamic
+setRepoPath = addShakeExtra . RepoPath
