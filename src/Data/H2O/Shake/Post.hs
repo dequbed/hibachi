f507a4f85e8c9b1b7da8822543a0bb64b6f3ab78
Nadja Reitzenstein <me@dequbed.space>
2020-04-14 11:12:28 +0200

    Clarify

--- src/Data/H2O/Shake/Post.hs
+++ src/Data/H2O/Shake/Post.hs
@@ -6,6 +6,7 @@ module Data.H2O.Shake.Post
 
 import qualified Prelude.List as L
 import qualified Prelude.Map as Map
+import qualified Prelude.ByteString as B
 
 import Development.Shake
 import Development.Shake.Rule
@@ -13,9 +14,11 @@ import Development.Shake.Rule
 import Data.H2O.Types (Post(..), story, storyName)
 import Data.H2O.Shake.Branch
 
+import Data.Maybe (fromJust)
+
 import Git.Types (SHA)
 
-newtype PostRule = PostRule (Post -> Action ())
+newtype PostRule = PostRule (Post -> Action FilePath)
     deriving Typeable
 
 type instance RuleResult PostQ = PostR
@@ -31,7 +34,9 @@ newtype PostR = PostR FilePath
 
 -- | Define a rule how to write a post.
 --   H2O knows *what* posts do build but not /how/. This is the way for an user to tell it.
-writePost :: (Post -> Action ()) -> Rules ()
+--   The rule needs to return the path where the post was written so link consistency can be
+--   enforced
+writePost :: (Post -> Action FilePath) -> Rules ()
 writePost act = addUserRule $ PostRule act
 
 -- | Get a List of all Posts in the index of the given branch, sorted newest to oldest
@@ -57,17 +62,19 @@ addPostBuildRule = addBuiltinRule noLint noIdentity run
         -- Get the user-defined `writePost` rule
         (ver, act) <- getUserRuleOne key disp test
         -- Use that rule to actually write the post
-        let a = act key
-        if mode == RunDependenciesSame && Just ver == (version <$> old) && postOid key == (blobOid <$> old) then
-            return $ RunResult ChangedNothing old $ PostR (path old)
+        let a = act (fromPostQ key)
+        if mode == RunDependenciesSame && Just ver == (version <$> old) && Just (postOid key) == (blobOid <$> old) then
+            return $ RunResult ChangedNothing (fromJust oldBin) $ PostR (path $ fromJust old)
         else do
-            new <- a
-            return $ RunResult ChangedRecomputeDiff new $ PostR (path new)
+            path <- a
+            let new = putEx $ PostA (postOid key) ver path
+            return $ RunResult ChangedRecomputeDiff new $ PostR path
 
--- Also, make user rules something to get from a TreeFilePath/Blob/Text to a Post? Res. patterns
--- over the kind of file so that users can define how .md and .tex are read into a post. (w/ low
--- `priority` default rules?)
---
 -- Two rules: WritePost that writes a Post, returns the path it was written to (important for url
 -- resolution). ReadPost that takes a TreeFilePath and generates a Post from that (so that users can
 -- use TeX or Markdown or RST instead of commonmark).
+
+getEx :: B.ByteString -> PostA
+getEx = undefined
+putEx :: PostA -> B.ByteString
+putEx = undefined
